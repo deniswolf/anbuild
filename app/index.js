05838e4007aa552f003cc07dda616fa4dfa2479b
Denis Wolf <dear.mr.dw@gmail.com>
2015-08-12 13:22:36 +0300

    add Angular app

--- app/index.js
+++ app/index.js
@@ -1,9 +1,45 @@
-module.exports = angular.module( 'app', [] );
+const app = angular.module( 'app', [] );
 // default params
-function printMessage( status = 'working' ) {
-  // let
-  const message = 'ES6';
-  // template string
-  console.log( `${message} is ${status}` );
-}
+function printMessage( status = 'working' ) {}
 printMessage();
+
+app.factory( 'AngelloHelper', function () {} );
+app.service( 'AngelloModel', function () {} );
+app.controller( 'MainCtrl', function () {
+  let main = this;
+  main.bubu = 122;
+
+  main.stories = [
+    {
+      title: 'First story',
+      description: 'Our first story.',
+      criteria: 'Criteria pending.',
+      status: 'To Do',
+      type: 'Feature',
+      reporter: 'Lukas Ruebbelke',
+      assignee: 'Brian Ford'
+        },
+    {
+      title: 'Second story',
+      description: 'Do something.',
+      criteria: 'Criteria pending.',
+      status: 'Back Log',
+      type: 'Feature',
+      reporter: 'Lukas Ruebbelke',
+      assignee: 'Brian Ford'
+        },
+    {
+      title: 'Another story',
+      description: 'Just one more.',
+      criteria: 'Criteria pending.',
+      status: 'Code Review',
+      type: 'Enhancement',
+      reporter: 'Lukas Ruebbelke',
+      assignee: 'Brian Ford'
+          }
+        ];
+
+} );
+app.directive( 'story', function () {} );
+
+module.exports = app;
